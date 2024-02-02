04ef1dccc67c27a7a46b7aebd2fdae4ff6a0db7d
Danil_Malin <malin-03@mail.ru>
2024-02-02 15:20:44 +0400

    Initial commit

---
+++ productWarehouse/src/main/java/com/task/productWarehouse/ProductWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.task.productWarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductWarehouseApplication.class, args);
+	}
+
+}
